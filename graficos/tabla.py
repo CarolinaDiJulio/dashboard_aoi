f108ad4f83480c41c92855d551fa937c7b567406
Carolina Di Julio <carolinadijulio@gmail.com>
2025-05-19 11:46:10 +0200

    tabla con los datos de los ultimos 7 dias

--- graficos/tabla.py
+++ graficos/tabla.py
@@ -4,4 +4,10 @@ import plotly.express as px
 
 def mostrar_tabla(): 
     df = pd.read_csv("datos_diarios.csv") 
-    st.dataframe(df) 
+    # Ordenar de más reciente a más antiguo
+    df = df.sort_values(by='Fecha', ascending=False)
+    # Filtrar las 7 fechas mas recientes
+    df_ultimos_7 = df.head(7)
+    df_ultimos_7 = df_ultimos_7.reset_index(drop=True)
+
+    st.dataframe(df_ultimos_7)
